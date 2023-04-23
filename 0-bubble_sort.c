1e89bdd05646fc47a06ffed82e01944a80f584fc
EnGentech <engen.inyang@gmail.com>
2023-04-22 23:13:30 -0700

    bubble sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - the array given to be sorted in ascending order
+ *
+ * @array: given the array to be sorted
+ * @size: size of the array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+int temp;
+size_t i, j;
+
+for (i = 0; i < size - 1; i++)
+{
+for (j = 0; j < size - i - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
+print_array(array, size);
+}
+}
+}
+}
