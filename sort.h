56b1d590deb40ed1db1dbdcac4155f1b34cba1ee
EnGentech <engen.inyang@gmail.com>
2023-04-26 15:34:28 -0700

    modified merge sort

--- sort.h
+++ sort.h
@@ -31,10 +31,15 @@ void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker);
 void counting_sort(int *array, size_t size);
 int lum_part(int *array, size_t size, int low, int high);
 void swp(int *i, int *j);
+void merge_sort(int *array, size_t size);
 void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
 void lum_sort(int *array, size_t size, int low, int high);
 void quick_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
+		size_t back);
+void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
+void merge_sort(int *array, size_t size);
 
 /* Coded by EnGentech and Ozonwu Frankelly */
 #endif
